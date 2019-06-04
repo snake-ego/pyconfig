1015f4868704033f0cd452e51d43335ba236e5eb
Igor Voropaev <snakework10@gmail.com>
2019-06-04 12:04:26 +0500

    Added setup for registry

---
+++ README.rst
@@ -0,0 +1,6 @@
+Configuration Formats Loader
+============================
+
+Бибилиотека для загрузки конфигурации в различных форматах в Python
+
+.. PyPi: https://pypi.itpc.dev/itpc/libs/configformat-loader
--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
-#!/usr/bin/env python
-import os
 import re
 from setuptools import setup, find_packages
+from os import path as op
 
 
-def get_version(fname):
+__version__ = '1.3'
+
+PACKAGES = find_packages(exclude=['tests'])
+NAME = PACKAGES[0]
+SCRIPTS = None
+DATA_FILES = None
+INCLUDE_PACKAGE_DATA = False
+
+
+patterns = {
+    'version': re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]'),
+    'doc': re.compile(r'__doc__ = [\'"]([^\'"]*)[\'"]')
+}
+
+
+def extract(pattern, fname):
     result = ''
-    reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
+    reg = re.compile(patterns[pattern])
     with open(fname, 'r') as fp:
         for line in fp:
             m = reg.match(line)
@@ -14,16 +28,25 @@ def get_version(fname):
                 result = m.group(1)
                 break
     if not result:
-        raise RuntimeError('Cannot find version information')
+        raise RuntimeError('Cannot find matched information for pattern {}'.format(pattern))
     return result
 
 
-__version__ = get_version("config/__init__.py")
+def get_readme(fname):
+    result = ''
+    if op.exists(fname):
+        with open(fname, encoding='utf-8') as f:
+            result = f.read()
+    return result
+
 
 setup(
-    name='Config-Control',
-    version=__version__,
-    description="Библиотека для работы с конфигурацией",
-    packages=find_packages(exclude=['tests']),
-    install_requires=open('requirements.txt').read() if os.path.exists('requirements.txt') else ''
+    version=extract('version', '{}/__init__.py'.format(NAME)),
+    description=extract('doc', '{}/__init__.py'.format(NAME)),
+    long_description=get_readme('README.md'),
+    install_requires=[],
+    packages=PACKAGES,
+    scripts=SCRIPTS,
+    data_files=DATA_FILES,
+    include_package_data=INCLUDE_PACKAGE_DATA
 )
